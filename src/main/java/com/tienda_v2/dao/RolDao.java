65c513fcfcc893b346bd0c153eab771e89339d80
Ghost <Ghost@Ghost>
2023-12-01 16:25:24 -0600

    Semana 11 Usuarios y Registro

---
+++ src/main/java/com/tienda_v2/dao/RolDao.java
@@ -0,0 +1,8 @@
+package com.tienda_v2.dao;
+
+import com.tienda_v2.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
---
+++ src/main/java/com/tienda_v2/service/CorreoService.java
@@ -0,0 +1,11 @@
+package com.tienda_v2.service;
+
+import jakarta.mail.MessagingException;
+
+public interface CorreoService {
+    public void enviarCorreoHtml(
+            String para, 
+            String asunto, 
+            String contenidoHtml) 
+            throws MessagingException;
+}
